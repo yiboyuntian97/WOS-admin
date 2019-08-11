bcc04902dce0ef10e9e4f0b481b3fdd6e831e98b
yiboyuntian97 <1121930180@qq.com>
2019-08-11 23:24:49 +0800

    修改变量命名及数据库port

--- src/main/java/com/wangyb/sell/repository/ProductCategoryRepository.java
+++ src/main/java/com/wangyb/sell/repository/ProductCategoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Integer> {
-    List<ProductCategory> findByCaAndCategoryTypeIn(List<Integer> categoryTypeList);
+    List<ProductCategory> findByCategoryTypeIn(List<Integer> categoryTypeList);
     ProductCategory findOne(Integer categoryId);
 
 }
