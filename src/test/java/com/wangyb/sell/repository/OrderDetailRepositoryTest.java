4fbba24797bb21e0d820db9712615f016ab28ef7
wangyibo <wangyibo@innser.sss.com>
2019-08-14 14:31:56 +0800

    修改字段命名错误+完成dao层测试

---
+++ src/test/java/com/wangyb/sell/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.wangyb.sell.repository;
+
+import com.wangyb.sell.dataObject.OrderDetail;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderDetailRepositoryTest {
+
+    @Autowired
+    private OrderDetailRepository  orderDetailRepository;
+
+    @Test
+    public void saveTest() {
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setDetailId("1234567890");
+        orderDetail.setOrderId("1111112");
+        orderDetail.setProductIcon("http://xxx.jpg");
+        orderDetail.setProductId("111222");
+        orderDetail.setProductName("鲤鱼");
+        orderDetail.setProductPrice(new BigDecimal(2.2));
+        orderDetail.setProductQuantity(3);
+        OrderDetail result = orderDetailRepository.save(orderDetail);
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void findByOrderId() throws  Exception {
+        List<OrderDetail> result = orderDetailRepository.findByOrOrderId("111111");
+        Assert.assertNotEquals(0,result.size());
+    }
+
+}
--- src/test/java/com/wangyb/sell/repository/OrderMasterRepositoryTest.java
+++ src/test/java/com/wangyb/sell/repository/OrderMasterRepositoryTest.java
@@ -6,11 +6,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.print.attribute.standard.PageRanges;
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OrderMasterRepositoryTest {
@@ -18,21 +22,25 @@ public class OrderMasterRepositoryTest {
     @Autowired
     private OrderMasterRepository repository;
 
+    private final String OPENID = "110110";
+
     @Test
     public void saveTest() {
         OrderMaster orderMaster = new OrderMaster();
-        orderMaster.setOrderId("123456");
+        orderMaster.setOrderId("1234567");
         orderMaster.setBuyerName("师兄");
         orderMaster.setBuyerPhone("13880088800");
         orderMaster.setBuyerAddress("上海市");
         orderMaster.setBuyerOpenid("110110");
-        orderMaster.setOrderAmount(new BigDecimal(2.3));
+        orderMaster.setOrderAmount(new BigDecimal(2.5));
         Assert.assertNotNull(repository.save(orderMaster));
     }
 
     @Test
-    public void  findByBuyerOpenid() throws Exception{
-
+    public void findByBuyerOpenid() throws Exception {
+        PageRequest request = new PageRequest(0, 1);
+        Page<OrderMaster> result = repository.findByBuyerOpenid(OPENID, request);
+        System.out.println(result.getTotalElements());
     }
 
 }
